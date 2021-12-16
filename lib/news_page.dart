6802aa44f14895f6deb1f9f790e45241defaae94
Abu Anwar <abuanwar072@gmail.com>
2021-12-16 16:36:25 +0600

    done

--- lib/news_page.dart
+++ lib/news_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:skelton/constants.dart';
+import 'package:skelton/skeleton.dart';
 
 import 'news_card.dart';
 
@@ -39,8 +40,11 @@ class _NewsPageState extends State<NewsPage> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
         child: _isLoading
-            ? const Center(
-                child: CircularProgressIndicator(),
+            ? ListView.separated(
+                itemCount: 5,
+                itemBuilder: (context, index) => const NewsCardSkelton(),
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: defaultPadding),
               )
             : ListView.separated(
                 itemCount: 6,
@@ -51,6 +55,46 @@ class _NewsPageState extends State<NewsPage> {
                     const SizedBox(height: defaultPadding),
               ),
       ),
+    );
+  }
+}
+
+class NewsCardSkelton extends StatelessWidget {
+  const NewsCardSkelton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Skeleton(height: 120, width: 120),
+        const SizedBox(width: defaultPadding),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Skeleton(width: 80),
+              const SizedBox(height: defaultPadding / 2),
+              const Skeleton(),
+              const SizedBox(height: defaultPadding / 2),
+              const Skeleton(),
+              const SizedBox(height: defaultPadding / 2),
+              Row(
+                children: const [
+                  Expanded(
+                    child: Skeleton(),
+                  ),
+                  SizedBox(width: defaultPadding),
+                  Expanded(
+                    child: Skeleton(),
+                  ),
+                ],
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
---
+++ lib/skeleton.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+class Skeleton extends StatelessWidget {
+  const Skeleton({Key? key, this.height, this.width}) : super(key: key);
+
+  final double? height, width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      padding: const EdgeInsets.all(defaultPadding / 2),
+      decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.04),
+          borderRadius:
+              const BorderRadius.all(Radius.circular(defaultPadding))),
+    );
+  }
+}
+
+class CircleSkeleton extends StatelessWidget {
+  const CircleSkeleton({Key? key, this.size = 24}) : super(key: key);
+
+  final double? size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size,
+      width: size,
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor.withOpacity(0.04),
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}
